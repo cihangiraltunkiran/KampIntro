6c7402628caf597e819004cea2e6770caddf9ff3
PC <PC@DESKTOP-KU84ILD>
2021-06-29 00:24:59 +0300

    Add project files.

---
+++ DegerVeReferansTipler/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace DegerVeReferansTipler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int sayi1 = 10;
+            int sayi2 = 30;
+            sayi1 = sayi2;
+            sayi2 = 65;
+            //sayi1 ?? 30
+
+            Console.WriteLine(sayi1);
+
+            Console.WriteLine("");
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine("");
+
+            int[] sayilar1 = new int[] { 10, 20, 30 };
+            int[] sayilar2 = new int[] { 100, 200, 300 };
+            sayilar1 = sayilar2;
+            sayilar2[0] = 999;
+            //sayilar1[0] ?? 999
+
+            Console.WriteLine(sayilar1[0]);
+
+            //int,decimal,float, double, bool = değer tip
+            //array, class, interface = referans tip
+
+            //         stack                    heap
+            // 101    sayilar1         101  [ 10,20,30 ]
+            // 102    sayilar2         102  [ 100,200,300 ]
+
+        }
+    }
+}
---
+++ KampIntro/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Type safety = Tip güvenliği
+            //Do not repeat yourself = Kendini tekrar etme
+            //Alias = Takma isim, değer tutucu
+
+            string kategoriEtiketi = "Kategori";
+            Console.WriteLine(kategoriEtiketi);
+
+            int ogrenciSayisi = 32000;
+            Console.WriteLine(ogrenciSayisi);
+
+            double faizOrani = 1.45;
+            Console.WriteLine(faizOrani);
+
+            bool sistemeGirisYapmisMi = true;
+            
+            if (sistemeGirisYapmisMi == true)
+            {
+                Console.WriteLine("Kullanıcı ayarları butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş yap butonu");
+            }
+
+            double dolarDun = 7.35;
+            double dolarBugun = 7.45;
+
+            if (dolarDun > dolarBugun)
+            {
+                Console.WriteLine("Azalış butonu");
+            }
+
+            else if (dolarDun < dolarBugun)
+            {
+                Console.WriteLine("Artış butonu");
+            }
+
+            else
+            {
+                Console.WriteLine("Eşittir butonu");
+            }
+
+
+
+
+
+        }
+    }
+}
